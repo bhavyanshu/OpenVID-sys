6a79b59693b148ac808f58f68ebf493c50eb5121
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2016-03-14 17:24:26 +0530

    Fixed seeders & removed o invalid views

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
-        //$this->call(RoleSeeder::class);
-        //$this->call(SUSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(SUSeeder::class);
         $this->call(NotificationCategorySeeder::class);
     }
 }
